d430cf5b3a7ec0612dbd113c41d9f2eac4681a90
Georgia Robinson <gldrobinson@hotmail.co.uk>
2021-12-28 16:56:14 +0000

    adds initial set up for xml and main files

---
+++ app/src/main/java/com/robinsgl/weatherapp/MainActivity.java
@@ -0,0 +1,38 @@
+package com.robinsgl.weatherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button findWeatherButton;
+    EditText citySearch;
+    TextView cityHeader;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findWeatherButton = findViewById(R.id.search_button);
+        citySearch = findViewById(R.id.city_search_edit_text);
+        cityHeader = findViewById(R.id.city_header_text_view);
+
+        findWeatherButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String toastMessage = "You searched for " + citySearch.getText().toString();
+                Log.i("citySearch", citySearch.getText().toString());
+                Toast toast = Toast.makeText(getApplicationContext(), toastMessage, Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+    }
+}
