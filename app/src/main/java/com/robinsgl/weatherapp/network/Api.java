8affef6c15e8a898790dd8a7934fd4b7db763674
Georgia Robinson <gldrobinson@hotmail.co.uk>
2021-12-28 17:03:20 +0000

    adds weather api interface and api class using retrofit

--- app/src/main/java/com/robinsgl/weatherapp/network/Api.java
+++ app/src/main/java/com/robinsgl/weatherapp/network/Api.java
@@ -1,2 +1,15 @@
-package com.robinsgl.weatherapp.network;public class Api {
+package com.robinsgl.weatherapp.network;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class Api {
+
+    public static WeatherApi create() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("api.openweathermap.org/data/2.5/weather")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        return retrofit.create(WeatherApi.class);
+    }
 }
