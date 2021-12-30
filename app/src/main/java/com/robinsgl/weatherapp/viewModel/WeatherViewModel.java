7bf5d7eeb5d59eb64d245721c89c0885994630e2
Georgia Robinson <gldrobinson@hotmail.co.uk>
2021-12-30 15:03:19 +0000

    adds weatherViewModel to call WeatherRepo

--- app/src/main/java/com/robinsgl/weatherapp/viewModel/WeatherViewModel.java
+++ app/src/main/java/com/robinsgl/weatherapp/viewModel/WeatherViewModel.java
@@ -1,2 +1,32 @@
-package com.robinsgl.weatherapp.viewModel;public class WeatherViewModel {
+package com.robinsgl.weatherapp.viewModel;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.robinsgl.weatherapp.data.OnWeatherResponse;
+import com.robinsgl.weatherapp.data.WeatherRepo;
+import com.robinsgl.weatherapp.model.WeatherModel;
+
+public class WeatherViewModel extends ViewModel {
+    private WeatherRepo weatherRepo;
+
+    public MutableLiveData<WeatherModel> weatherData = new MutableLiveData<>();
+
+    public WeatherViewModel() {
+        this.weatherRepo = new WeatherRepo();
+    }
+
+    public void findWeatherByCity(String cityName) {
+        weatherRepo.getWeather(cityName, new OnWeatherResponse() {
+            @Override
+            public void onWeatherReceived(WeatherModel weather) {
+                weatherData.setValue(weather);
+            }
+
+            @Override
+            public void onWeatherError(Exception exception) {
+                weatherData.setValue(new WeatherModel());
+            }
+        });
+    }
 }
